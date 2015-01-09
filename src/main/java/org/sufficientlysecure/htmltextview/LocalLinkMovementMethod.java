1930d53b4928bfc36f03f94a0bfe0f81a1d454d9
heliangwei <heliangwei@hujiang.com>
2015-01-09 13:36:26 +0800

    modify: LinkMovementMethod makes list item unclickable

---
+++ src/main/java/org/sufficientlysecure/htmltextview/LocalLinkMovementMethod.java
@@ -0,0 +1,68 @@
+package com.hujiang.android.uikit.htmltextview;
+
+import android.text.Layout;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.text.method.Touch;
+import android.text.style.ClickableSpan;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+/**
+ * Created by heliangwei on 2015/1/9.
+ */
+public  class LocalLinkMovementMethod extends LinkMovementMethod {
+    static LocalLinkMovementMethod sInstance;
+
+
+    public static LocalLinkMovementMethod getInstance() {
+        if (sInstance == null)
+            sInstance = new LocalLinkMovementMethod();
+
+        return sInstance;
+    }
+
+    @Override
+    public boolean onTouchEvent(TextView widget, Spannable buffer, MotionEvent event) {
+        int action = event.getAction();
+
+        if (action == MotionEvent.ACTION_UP ||
+                action == MotionEvent.ACTION_DOWN) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+
+            x -= widget.getTotalPaddingLeft();
+            y -= widget.getTotalPaddingTop();
+
+            x += widget.getScrollX();
+            y += widget.getScrollY();
+
+            Layout layout = widget.getLayout();
+            int line = layout.getLineForVertical(y);
+            int off = layout.getOffsetForHorizontal(line, x);
+
+            ClickableSpan[] link = buffer.getSpans(off, off, ClickableSpan.class);
+
+            if (link.length != 0) {
+                if (action == MotionEvent.ACTION_UP) {
+                    link[0].onClick(widget);
+                } else if (action == MotionEvent.ACTION_DOWN) {
+                    Selection.setSelection(buffer,
+                            buffer.getSpanStart(link[0]),
+                            buffer.getSpanEnd(link[0]));
+                }
+
+                if (widget instanceof HtmlTextView){
+                    ((HtmlTextView) widget).linkHit = true;
+                }
+                return true;
+            } else {
+                Selection.removeSelection(buffer);
+                Touch.onTouchEvent(widget, buffer, event);
+                return false;
+            }
+        }
+        return Touch.onTouchEvent(widget, buffer, event);
+    }
+}
