e73e1a94e40976be3ef7ddc87426405b07be033a
heliangwei <heliangwei@hujiang.com>
2015-02-06 22:06:50 +0800

    modify: add Apache license comment

--- src/main/java/org/sufficientlysecure/htmltextview/LocalLinkMovementMethod.java
+++ src/main/java/org/sufficientlysecure/htmltextview/LocalLinkMovementMethod.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Heliangwei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.sufficientlysecure.htmltextview;
 
 import android.text.Layout;
@@ -10,7 +26,7 @@ import android.view.MotionEvent;
 import android.widget.TextView;
 
 /**
- * Created by heliangwei on 2015/1/9.
+ * Copied from http://stackoverflow.com/questions/8558732
  */
 public  class LocalLinkMovementMethod extends LinkMovementMethod {
     static LocalLinkMovementMethod sInstance;
