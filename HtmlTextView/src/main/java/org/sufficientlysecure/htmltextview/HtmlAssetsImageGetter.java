d1e673d9d4e58393f4a1d85dbf278763be40daf0
Daniel Passos <daniel@passos.me>
2016-08-18 16:51:11 -0300

    Add license header

--- HtmlTextView/src/main/java/org/sufficientlysecure/htmltextview/HtmlAssetsImageGetter.java
+++ HtmlTextView/src/main/java/org/sufficientlysecure/htmltextview/HtmlAssetsImageGetter.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014-2016 Dominik Schürmann <dominik@dominikschuermann.de>
+ * Copyright (C) 2014 drawk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.sufficientlysecure.htmltextview;
 
 import android.content.Context;
@@ -11,7 +28,7 @@ import java.io.InputStream;
 
 /**
  * Assets Image Getter
- * 
+ *
  * Load image from assets folder
  *
  * @author <a href="mailto:daniel@passos.me">Daniel Passos</a>
