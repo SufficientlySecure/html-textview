c246e513c7f02f27319edc3a5fdf0a9869e9f377
Olivér Falvai <ofalvai@gmail.com>
2017-05-28 18:44:29 +0200

    Removed unused boolean fields and onTouchEvent() to fix text selection

--- HtmlTextView/src/main/java/org/sufficientlysecure/htmltextview/HtmlTextView.java
+++ HtmlTextView/src/main/java/org/sufficientlysecure/htmltextview/HtmlTextView.java
@@ -22,7 +22,6 @@ import android.support.annotation.Nullable;
 import android.support.annotation.RawRes;
 import android.text.Html;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
 
 import java.io.InputStream;
 import java.util.Scanner;
@@ -32,13 +31,11 @@ public class HtmlTextView extends JellyBeanSpanFixTextView {
     public static final String TAG = "HtmlTextView";
     public static final boolean DEBUG = false;
 
-    boolean linkHit;
     @Nullable
     private ClickableTableSpan clickableTableSpan;
     @Nullable
     private DrawTableLinkSpan drawTableLinkSpan;
 
-    boolean dontConsumeNonUrlClicks = true;
     private boolean removeFromHtmlSpace = true;
 
     public HtmlTextView(Context context, AttributeSet attrs, int defStyle) {
@@ -147,16 +144,4 @@ public class HtmlTextView extends JellyBeanSpanFixTextView {
         }
         return text;
     }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        linkHit = false;
-        boolean res = super.onTouchEvent(event);
-
-        if (dontConsumeNonUrlClicks) {
-            return linkHit;
-        }
-        return res;
-    }
-
 }
